5435d00979bfc565597dfa27f642b3d588848452
Dan Ross-Li <me@danrossli.com>
2025-07-04 09:08:22 -0400

    use @ reference

--- src/app/to/[formId]/page.jsx
+++ src/app/to/[formId]/page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { getForm } from "@/services/form";
-import FormClient from "../../../components/FormClient";
+import FormClient from "@/components/form/FormClient";
 
 export default async function FormPage({ params }) {
   const { formId } = await params;
