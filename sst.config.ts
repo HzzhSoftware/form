c61eafdae86a1192b074aba670ab28fc79c85a08
Dan Ross-Li <me@danrossli.com>
2025-09-18 14:17:09 -0400

    add dynamic subdomain

--- sst.config.ts
+++ sst.config.ts
@@ -5,18 +5,19 @@ export default $config({
   app(input) {
     return {
       name: "form-fe",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      protect: ["production"].includes(input?.stage),
+      removal: "remove",
+      //protect: isProd,
       home: "aws",
     };
   },
   async run() {
     const hostedZoneId = process.env.HOSTED_ZONE_ID;
     const certificateArn = process.env.CERTIFICATE_ARN;
+    const subdomain = process.env.SUBDOMAIN;
 
     new sst.aws.Nextjs("Form", {
       domain: {
-        name: "form.kycombinator.com",
+        name: `${subdomain}.kycombinator.com`,
         dns: sst.aws.dns({
           zone: hostedZoneId
         }), // Add a DNS record to the domain
